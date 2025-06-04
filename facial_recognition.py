26c8a8f769d834643bcf9d8d090923baa6e66f22
kirill <lamsquirrel75@gmail.com>
2025-06-04 17:56:46 +0100

    polishing touch

--- facial_recognition.py
+++ facial_recognition.py
@@ -116,7 +116,7 @@ class FaceRecognizer:
                     # If recognized in specified amount of frames, return true
                     match_counter += 1
                     self.name = face_names
-                    j/?
+
                     #print(self.name)
                     #print(match_counter)
                     if match_counter >= match_needed:
